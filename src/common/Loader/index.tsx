d8717c3ddbecc0c8bc1a2f118959ae9b157e4423
iamtrazy <iamtrazy@proton.me>
2024-06-27 22:05:13 +0530

    fix: loader darkmode

--- src/common/Loader/index.tsx
+++ src/common/Loader/index.tsx
@@ -1,6 +1,9 @@
+import useColorMode from "../../hooks/useColorMode";
+
 const Loader = () => {
+  useColorMode();
   return (
-    <div className="flex h-screen items-center justify-center bg-transparent">
+    <div className="flex h-screen items-center justify-center dark:bg-meta-4 bg-white">
       <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-yellow-500 border-t-transparent"></div>
     </div>
   );
