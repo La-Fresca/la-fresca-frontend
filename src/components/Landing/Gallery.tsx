f20effed41dbf2489da4d4a0c082973fa521dfb5
Ravindu Hasanka <rhatu2000@gmail.com>
2024-07-09 17:45:13 +0530

    image slider added

--- src/components/Landing/Gallery.tsx
+++ src/components/Landing/Gallery.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+"use client";
+import { motion } from "framer-motion";
+import React from "react";
+import { ImagesSlider } from "../../components/Landing/images-slider";
 
 const Gallery = () => {
-  return (
-    <div>gallery</div>
-  )
+    const images = [
+        "https://images.unsplash.com/photo-1509042239860-f550ce710b93?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "https://images.unsplash.com/photo-1445116572660-236099ec97a0?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      ];
+      return (
+        <ImagesSlider className="h-screen" images={images}>
+          <motion.div
+            initial={{
+              opacity: 0,
+              y: -80,
+            }}
+            animate={{
+              opacity: 1,
+              y: 0,
+            }}
+            transition={{
+              duration: 0.6,
+            }}
+            className="z-50 flex flex-col justify-center items-center"
+          >
+            <motion.p className="font-bold text-xl md:text-6xl text-start bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
+              {/* Image Gallery */}
+            </motion.p>
+            {/* <button className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
+              <span>View More</span>
+              <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
+            </button> */}
+          </motion.div>
+        </ImagesSlider>
+      );
 }
 
 export default Gallery
