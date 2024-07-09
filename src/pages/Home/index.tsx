87fee1c4a3cea2e18444ce82c6de319b2d9db574
Ravindu Hasanka <rhatu2000@gmail.com>
2024-07-09 17:29:21 +0530

    Gallery creation started

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,11 @@
+import React from 'react';
+import Gallery from '@components/Landing/gallery';
+
 const Home: React.FC = () => {
     return (
         <div className="dark:bg-black bg-white">
             <h1>Home</h1>
+            <Gallery />
         </div>
     );
 }
