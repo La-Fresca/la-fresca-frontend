019ce9393d264ec494eaa3ae5b5a297bf5e02b2f
Ravindu Hasanka <rhatu2000@gmail.com>
2024-07-10 12:15:06 +0530

    enh: Enhance in titlebox

--- src/components/Landing/Menus.tsx
+++ src/components/Landing/Menus.tsx
@@ -17,7 +17,7 @@ const Menus = () => {
                 title={""}
                 subtitle={""}
                 button={""}
-                ClassName={"absolute left-0 top-60 left-10 w-[50%] h-[60%] bg-cover bg-center bg-[url('https://images.unsplash.com/photo-1414235077428-338989a2e8c0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]"}
+                ClassName={"absolute left-0 top-70 left-10 w-[50%] h-[55%] bg-cover bg-center bg-[url('https://images.unsplash.com/photo-1414235077428-338989a2e8c0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]"}
                 URLLink=""
             />
 
--- src/components/Landing/TitleBox.tsx
+++ src/components/Landing/TitleBox.tsx
@@ -15,10 +15,10 @@ export const TitleBox: React.FC<TitleBoxProps> = ({ title, subtitle, button, Cla
 
   return (
     <div className={boxClass}>
-      <h3 className="text-xl text-black font-semibold uppercase font-inter text-center">{subtitle}</h3>
-      <h1 className="text-8xl text-black uppercase font-noto-serif justify-self-center text-center">{title}</h1>
+      <h3 className="text-xl text-black font-semibold uppercase font-inter text-center sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">{subtitle}</h3>
+      <h1 className="text-6xl lg:text-8xl text-black uppercase font-noto-serif text-center">{title}</h1>
       {button && (
-        <Link to={URLLink} className="text-black text-base font-bold uppercase font-inter flex items-center justify-center">
+        <Link to={URLLink} className="text-black text-base font-bold uppercase font-inter flex items-center justify-center mt-4">
           <FiPlus className="mr-2" /> {button}
         </Link>
       )}
