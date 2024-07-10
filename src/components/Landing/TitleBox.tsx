747741bf5d04eae898e8237d7a28f53993124380
Ravindu Hasanka <rhatu2000@gmail.com>
2024-07-10 10:20:55 +0530

    Menu created

--- src/components/Landing/TitleBox.tsx
+++ src/components/Landing/TitleBox.tsx
@@ -2,20 +2,26 @@ import React from 'react';
 import { FiPlus } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 
-export const TitleBox = ({ title, subtitle, button, ClassName, URLLink }: { title: string, subtitle: string, button: string, ClassName: string, URLLink:string }) => {
-    const boxClass = `${ClassName} bg-yellow-500 flex flex-col items-center justify-center tracking-widest p-4`;
-    
-    return (
-        // <div className="relative bg-gray-200 h-screen flex items-center justify-center">
-        // <div className="w-screen h-screen bg-blue-500 flex items-end justify-end">
-        <div className={boxClass}>
-            <h3 className="text-xl text-black font-semibold uppercase font-inter">{subtitle}</h3>
-            <h1 className="text-8xl text-black uppercase font-noto-serif">{title}</h1>
-            <Link to={URLLink} className="text-black text-base font-bold uppercase font-inter flex items-center">
-                <FiPlus className="mr-2" /> View More
-            </Link>
-        </div>
-        // </div>
-        // </div>
-    );
+interface TitleBoxProps {
+  title: string;
+  subtitle: string;
+  button: string;
+  ClassName: string;
+  URLLink: string;
+}
+
+export const TitleBox: React.FC<TitleBoxProps> = ({ title, subtitle, button, ClassName, URLLink }) => {
+  const boxClass = `${ClassName} bg-yellow-500 flex flex-col items-center justify-center tracking-widest p-4`;
+
+  return (
+    <div className={boxClass}>
+      <h3 className="text-xl text-black font-semibold uppercase font-inter">{subtitle}</h3>
+      <h1 className="text-8xl text-black uppercase font-noto-serif">{title}</h1>
+      {button && (
+        <Link to={URLLink} className="text-black text-base font-bold uppercase font-inter flex items-center">
+          <FiPlus className="mr-2" /> {button}
+        </Link>
+      )}
+    </div>
+  );
 };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Gallery from '@components/Landing/Gallery';
+import Menus from '@components/Landing/Menus';
 
 const Home: React.FC = () => {
     return (
         <div className="dark:bg-black bg-white">
             <h1>Home</h1>
             <Gallery />
+            <Menus />
         </div>
     );
 }
