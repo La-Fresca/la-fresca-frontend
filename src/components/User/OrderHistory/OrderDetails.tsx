28948dd0be5e45556ce6723d23eff29a4f3cd130
Ravindu Hasanka <rhatu2000@gmail.com>
2024-07-12 03:13:47 +0530

    order item card done

--- src/components/User/OrderHistory/OrderDetails.tsx
+++ src/components/User/OrderHistory/OrderDetails.tsx
@@ -1,26 +1,38 @@
 import React from 'react'
 import { TbTruckDelivery } from "react-icons/tb";
 import CustomizedSteppers from './Stepping';
+import { OrderItem } from './OrderItem';
 
 export const OrderDetails = () => {
   return (
     <div className="">
-            <div
-                className="w-[100%] flex flex-col lg:flex-col flex-grow items-left align-middle justify-between my-2 px-4 py-4 rounded-2xl border border-foodbg bg-foodbg  backdrop-blur-md"
-                style={{
-                    backgroundColor: 'rgba(255, 255, 255, 0.01)',
-                }}
-            >
-                <h1 className='text-2xl text-white'>Order No : #3222110</h1>
-                <div className='flex justify-left gap-2 flex-row mb-5'>
-                    <p className='text-white'>Order Date : July 11, 2024</p>
-                    <p className='text-white'>|</p>
-                    <TbTruckDelivery className='text-white text-2xl' />
-                    <p className='text-white'>Estimated delivery: May 16, 2022</p>
-                </div>
-                <CustomizedSteppers />
-
-            </div>
+      <div
+        className="w-[100%] flex flex-col lg:flex-col flex-grow items-left align-middle justify-between my-2 px-4 py-4 rounded-2xl border border-foodbg bg-foodbg  backdrop-blur-md"
+        style={{
+          backgroundColor: 'rgba(255, 255, 255, 0.01)',
+        }}
+      >
+        <h1 className='text-2xl text-white'>Order No : #3222110</h1>
+        <div className='flex justify-left gap-2 flex-row mb-5'>
+          <p className='text-white'>Order Date : July 11, 2024</p>
+          <p className='text-white'>|</p>
+          <TbTruckDelivery className='text-white text-2xl' />
+          <p className='text-white'>Estimated delivery: May 16, 2022</p>
         </div>
+        <CustomizedSteppers />
+        <div className='flex flex-col gap-2 mx-20 mt-10'>
+          {/* <div className='flex flex-row justify-between'>
+            <p className='text-white'>Items</p>
+            <p className='text-white'>Price</p>
+          </div> */}
+          <div className=' w-full flex flex-col'>
+          <OrderItem />
+          <OrderItem />
+          <OrderItem />
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
--- src/components/User/OrderHistory/OrderItem.tsx
+++ src/components/User/OrderHistory/OrderItem.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 export const OrderItem = () => {
-  return (
-    <div>OrderItem</div>
-  )
+    return (
+        <div className='w-full flex flex-row justify-between items-center p-4 bg-gray-800'>
+            <div className='w-1/6'>
+                <img src='https://via.placeholder.com/150' alt='food' className='w-full h-auto object-cover' />
+            </div>
+            <div className='w-3/6 mx-5 flex-grow'>
+                <p className='text-white text-xl font-bold'>Food Name</p>
+                <p className='text-white'>Food Description</p>
+            </div>
+            <div className='w-2/6 flex flex-col items-end'>
+                <p className='text-white text-xl font-semibold'>Price</p>
+                <p className='text-white text-base font-normal'>Quantity</p>
+            </div>
+        </div>
+    );
 }
