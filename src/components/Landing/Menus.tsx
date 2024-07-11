02f31c0e612d4d2d6110a9c8baa709dbccd7add1
Ravindu Hasanka <rhatu2000@gmail.com>
2024-07-11 13:42:50 +0530

    Update

--- src/components/Landing/Menus.tsx
+++ src/components/Landing/Menus.tsx
@@ -18,7 +18,7 @@ const Menus = () => {
                 subtitle={""}
                 button={""}
                 ClassName={"absolute left-0 top-70 left-10 w-[50%] h-[55%] bg-cover bg-center bg-[url('https://images.unsplash.com/photo-1414235077428-338989a2e8c0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]"}
-                URLLink=""
+                URLLink="/fooditems"
             />
 
         </div>
