c325ecded04f6823ecd649157801109acea1821e
Ravindu Hasanka <rhatu2000@gmail.com>
2024-07-12 02:54:47 +0530

    order item started

--- src/components/User/OrderHistory/OrderDetails.tsx
+++ src/components/User/OrderHistory/OrderDetails.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { TbTruckDelivery } from "react-icons/tb";
+import CustomizedSteppers from './Stepping';
 
 export const OrderDetails = () => {
   return (
-    <div>OrderDetails</div>
+    <div className="">
+            <div
+                className="w-[100%] flex flex-col lg:flex-col flex-grow items-left align-middle justify-between my-2 px-4 py-4 rounded-2xl border border-foodbg bg-foodbg  backdrop-blur-md"
+                style={{
+                    backgroundColor: 'rgba(255, 255, 255, 0.01)',
+                }}
+            >
+                <h1 className='text-2xl text-white'>Order No : #3222110</h1>
+                <div className='flex justify-left gap-2 flex-row mb-5'>
+                    <p className='text-white'>Order Date : July 11, 2024</p>
+                    <p className='text-white'>|</p>
+                    <TbTruckDelivery className='text-white text-2xl' />
+                    <p className='text-white'>Estimated delivery: May 16, 2022</p>
+                </div>
+                <CustomizedSteppers />
+
+            </div>
+        </div>
   )
 }
--- src/components/User/OrderHistory/OrderHistory.tsx
+++ src/components/User/OrderHistory/OrderHistory.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { OrderCard } from './OrderCard'
+import { OrderDetails } from './OrderDetails'
 
 export const OrderHistory = () => {
   return (
     <div className='flex flex-row w-screen px-10'>
-      <div className='flex flex-col mx-10'>
+      <div className='w-[40%] flex flex-col mx-10'>
       <OrderCard />
       <OrderCard />
       <OrderCard />
       </div>
-      <div>
-      <OrderCard />
+      <div className='w-[60%] flex flex-col mx-10'>
+      <OrderDetails />
       </div>
       
     </div>
