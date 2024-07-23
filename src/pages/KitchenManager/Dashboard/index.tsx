e08ea0eb8941ede496e1d8592b3bea5eb310f529
DasunThathsara <dasun.thathsara.sri@gmail.com>
2024-07-23 19:44:40 +0530

    temp

--- src/pages/KitchenManager/Dashboard/index.tsx
+++ src/pages/KitchenManager/Dashboard/index.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import ChartThree from '@components/BranchManager/Charts/ChartThree';
-import ChartFour from '@/components/BranchManager/Charts/ChartFour';
-import ChartFive from '@/components/BranchManager/Charts/ChartFive';
-import TableOne from '@/components/BranchManager/Tables/TableOne';
-import ChartSix from '@/components/BranchManager/Charts/ChartSix';
 
 const Dashboard: React.FC = () => {
   return (
     <>
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
-        <ChartFour />
-        <ChartThree />
-        <ChartFive />
-        <TableOne />
-        <ChartSix />
+        <p>Hello</p>
       </div>
     </>
   );
