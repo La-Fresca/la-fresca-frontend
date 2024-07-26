3b4f9cc56d88423777a79d284280bad56add3472
W.M.U.I.WANASINGHA <144322302+indusara1214@users.noreply.github.com>
2024-07-26 11:34:28 +0530

    Login and resgistration forms

---
+++ src/pages/User/Registration.tsx
@@ -0,0 +1,54 @@
+import LoginForm from '@components/User/RegistrationForm';
+import backgroundImage from '@images/cover/Logincover.png';
+import LogoIcon from '@/images/logo/la-fresca.png';
+
+const LoginPage = () => {
+  return (
+    
+    <div className="relative h-screen overflow-hidden">
+      {/* Background image */}
+      <div className="absolute inset-0 ">
+        <img src={backgroundImage} alt="Background" />
+      </div>
+
+      {/* Content */}
+      {/* Hidden on mobile */}
+  <div className='flex '>
+
+    <div className="flex flex-col w-1/2 z-1">
+        <div className="mt-30 h-60 justify-center flex w-full">
+            <img src={LogoIcon} alt="Background" />        
+        </div>
+
+        <div className='mt-10 text-center'>
+            <h1 className='text-xl font-bold'>
+            Already have an account?
+            </h1>
+            <p className='my-5 mx-25'>
+            Register now to access personalized content, save your preferences, and enjoy exclusive discounts. Click the button to get started!
+            </p>
+            <button className='bg-yellow-500 text-white rounded-md px-5 py-2'>
+            Login
+            </button>
+        </div>       
+        </div>
+
+    <div className="hidden w-1/2 md:flex md:items-center md:justify-center bg-white bg-opacity-20 backdrop-blur relative">
+        <div className="w-2/3 p-6 m-auto bg-transparent bg-opacity-0 backdrop-blur-none rounded-md shadow-none lg:max-w-lg">
+          <LoginForm />
+        </div>
+    </div>  
+  </div>
+
+
+      {/* Shown on mobile */}
+      <div className="md:hidden">
+        <div className="max-w-lg mx-auto p-8 bg-white bg-opacity-30 backdrop-blur-lg rounded-lg shadow-lg">
+          <LoginForm />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
