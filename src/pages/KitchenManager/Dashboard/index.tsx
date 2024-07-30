b7e912a55b1d672ae50194d7594642afb60bfeb7
DasunThathsara <dasun.thathsara.sri@gmail.com>
2024-07-30 19:56:34 +0530

    fix:  kitchen manager dash

--- src/pages/KitchenManager/Dashboard/index.tsx
+++ src/pages/KitchenManager/Dashboard/index.tsx
@@ -3,266 +3,98 @@ import { Button } from '@nextui-org/react';
 
 const Dashboard: React.FC = () => {
   const colours = ['red', 'green', 'blue', 'yellow', 'white'];
-  const itemWaitingQueue = [
+  const orders = [
     {
       id: '01',
       orderId: 102,
+      orderType: 'ONLINE',
       time: '2',
-      items: [
+      orderStatus: 'PENDING',
+      foodItems: [
         {
-          id: '01',
+          foodId: '01',
           name: 'Cheese Pizza',
-          description: 'Indulge in our...',
           quentity: 2,
-          price: 3500,
+          itemStatus: 'PENDING',
         },
         {
-          id: '02',
+          foodId: '02',
+          name: 'Chicken Pizza',
+          quentity: 2,
+          itemStatus: 'PENDING',
+        },
+        {
+          foodId: '03',
           name: 'Saussage Pizza',
-          description: 'Indulge in our...',
           quentity: 5,
-          price: 4500,
+          itemStatus: 'PREPARING',
         },
       ],
     },
     {
       id: '02',
       orderId: 105,
-      time: '10',
-      items: [
-        {
-          id: '01',
-          name: 'Cheese Pizza',
-          description: 'Indulge in our...',
-          quentity: 1,
-          price: 3500,
-        },
-      ],
-    },
-    {
-      id: '02',
-      orderId: 105,
-      time: '10',
-      items: [
-        {
-          id: '01',
-          name: 'Cheese Pizza',
-          description: 'Indulge in our...',
-          quentity: 1,
-          price: 3500,
-        },
-      ],
-    },
-    {
-      id: '02',
-      orderId: 105,
-      time: '10',
-      items: [
-        {
-          id: '01',
-          name: 'Cheese Pizza',
-          description: 'Indulge in our...',
-          quentity: 1,
-          price: 3500,
-        },
-      ],
-    },
-    {
-      id: '01',
-      orderId: 102,
+      orderType: 'ONLINE',
       time: '2',
-      items: [
+      orderStatus: 'PENDING',
+      foodItems: [
         {
-          id: '01',
+          foodId: '01',
           name: 'Cheese Pizza',
-          description: 'Indulge in our...',
           quentity: 2,
-          price: 3500,
+          itemStatus: 'PENDING',
         },
         {
-          id: '02',
+          foodId: '03',
           name: 'Saussage Pizza',
-          description: 'Indulge in our...',
           quentity: 5,
-          price: 4500,
+          itemStatus: 'PREPARING',
+        },
+        {
+          foodId: '03',
+          name: 'Saussage Pizza',
+          quentity: 5,
+          itemStatus: 'READY',
         },
       ],
     },
   ];
 
-  const itemPendingQueue = [
-    {
-      id: '01',
-      orderId: 102,
-      time: '2',
-      items: [
-        {
-          id: '01',
-          name: 'Cheese Pizza',
-          description: 'Indulge in our...',
-          quentity: 2,
-          price: 3500,
-        },
-        {
-          id: '02',
-          name: 'Saussage Pizza',
-          description: 'Indulge in our...',
-          quentity: 5,
-          price: 4500,
-        },
-      ],
-    },
-    {
-      id: '02',
-      orderId: 105,
-      time: '10',
-      items: [
-        {
-          id: '01',
-          name: 'Cheese Pizza',
-          description: 'Indulge in our...',
-          quentity: 1,
-          price: 3500,
-        },
-      ],
-    },
-    {
-      id: '02',
-      orderId: 105,
-      time: '10',
-      items: [
-        {
-          id: '01',
-          name: 'Cheese Pizza',
-          description: 'Indulge in our...',
-          quentity: 1,
-          price: 3500,
-        },
-      ],
-    },
-    {
-      id: '02',
-      orderId: 105,
-      time: '10',
-      items: [
-        {
-          id: '01',
-          name: 'Cheese Pizza',
-          description: 'Indulge in our...',
-          quentity: 1,
-          price: 3500,
-        },
-      ],
-    },
-    {
-      id: '01',
-      orderId: 102,
-      time: '2',
-      items: [
-        {
-          id: '01',
-          name: 'Cheese Pizza',
-          description: 'Indulge in our...',
-          quentity: 2,
-          price: 3500,
-        },
-        {
-          id: '02',
-          name: 'Saussage Pizza',
-          description: 'Indulge in our...',
-          quentity: 5,
-          price: 4500,
-        },
-      ],
-    },
-  ];
+  const itemWaitingQueue = orders
+    .filter((order) => order.orderStatus === 'PENDING')
+    .map((order) => ({
+      ...order,
+      foodItems: order.foodItems.filter(
+        (foodItem) => foodItem.itemStatus === 'PENDING',
+      ),
+    }))
+    .filter((order) => order.foodItems.length > 0);
 
-  const itemCompletedQueue = [
-    {
-      id: '01',
-      orderId: 102,
-      time: '2',
-      items: [
-        {
-          id: '01',
-          name: 'Cheese Pizza',
-          description: 'Indulge in our...',
-          quentity: 2,
-          price: 3500,
-        },
-        {
-          id: '02',
-          name: 'Saussage Pizza',
-          description: 'Indulge in our...',
-          quentity: 5,
-          price: 4500,
-        },
-      ],
-    },
-    {
-      id: '02',
-      orderId: 105,
-      time: '10',
-      items: [
-        {
-          id: '01',
-          name: 'Cheese Pizza',
-          description: 'Indulge in our...',
-          quentity: 1,
-          price: 3500,
-        },
-      ],
-    },
-    {
-      id: '02',
-      orderId: 105,
-      time: '10',
-      items: [
-        {
-          id: '01',
-          name: 'Cheese Pizza',
-          description: 'Indulge in our...',
-          quentity: 1,
-          price: 3500,
-        },
-      ],
-    },
-    {
-      id: '02',
-      orderId: 105,
-      time: '10',
-      items: [
-        {
-          id: '01',
-          name: 'Cheese Pizza',
-          description: 'Indulge in our...',
-          quentity: 1,
-          price: 3500,
-        },
-      ],
-    },
-    {
-      id: '01',
-      orderId: 102,
-      time: '2',
-      items: [
-        {
-          id: '01',
-          name: 'Cheese Pizza',
-          description: 'Indulge in our...',
-          quentity: 2,
-          price: 3500,
-        },
-        {
-          id: '02',
-          name: 'Saussage Pizza',
-          description: 'Indulge in our...',
-          quentity: 5,
-          price: 4500,
-        },
-      ],
-    },
-  ];
+  const itemPendingQueue = orders
+    .filter(
+      (order) =>
+        order.orderStatus === 'PENDING' || order.orderStatus === 'PREPARING',
+    )
+    .map((order) => ({
+      ...order,
+      foodItems: order.foodItems.filter(
+        (foodItem) => foodItem.itemStatus === 'PREPARING',
+      ),
+    }))
+    .filter((order) => order.foodItems.length > 0);
+
+  const itemCompletedQueue = orders
+    .filter(
+      (order) =>
+        order.orderStatus === 'PENDING' || order.orderStatus === 'PREPARING',
+    )
+    .map((order) => ({
+      ...order,
+      foodItems: order.foodItems.filter(
+        (foodItem) => foodItem.itemStatus === 'READY',
+      ),
+    }))
+    .filter((order) => order.foodItems.length > 0);
 
   var qCount = 0;
   var pCount = 0;
@@ -291,13 +123,12 @@ const Dashboard: React.FC = () => {
                     Order ID: {_.orderId}
                   </p>
                   <p className="text-center text-xs"> {_.time} min ago</p>
-                  {_.items.map((item: any) => {
+                  {_.foodItems.map((item: any) => {
                     return (
                       <div className="border border-foodbg rounded-xl py-2 px-2 flex items-center justify-between mt-5">
                         <div className="w-[60px] h-[60px] bg-green"></div>
                         <div className="w-[110px] h-[60px]">
                           <p className="text-md">{item.name}</p>
-                          <p className="text-sm">{item.description}</p>
                           <p className="text-xs">x{item.quentity}</p>
                         </div>
                         <div className="grid w-[100px]">
@@ -322,32 +153,28 @@ const Dashboard: React.FC = () => {
           }}
         >
           <p className="font-bold dark:text-white text-foodbg text-2xl pt-5 text-center">
-            Proccessing
+            Processing
           </p>
           <div className="width-[100%] mt-2 mx-4 !overflow-scroll h-[70vh] px-2">
             {itemPendingQueue.map((_: any) => {
               return (
                 <div
                   className={`rounded-2xl px-2 py-2 mt-5`}
-                  style={{ boxShadow: `0 0 10px 0.1px ${colours[pCount]}` }}
+                  style={{ boxShadow: `0 0 10px 0.1px ${colours[pCount]} ` }}
                 >
                   <p className="text-center text-foodbg dark:text-white text-lg font-bold">
                     Order ID: {_.orderId}
                   </p>
                   <p className="text-center text-xs"> {_.time} min ago</p>
-                  {_.items.map((item: any) => {
+                  {_.foodItems.map((item: any) => {
                     return (
                       <div className="border border-foodbg rounded-xl py-2 px-2 flex items-center justify-between mt-5">
                         <div className="w-[60px] h-[60px] bg-green"></div>
                         <div className="w-[110px] h-[60px]">
                           <p className="text-md">{item.name}</p>
-                          <p className="text-sm">{item.description}</p>
                           <p className="text-xs">x{item.quentity}</p>
                         </div>
                         <div className="grid w-[100px]">
-                          <Button className="bg-gradient-to-r from-orange-600 to-orange-400 text-white shadow-lg rounded-lg text-xl scale-50 py-5 px-10 mb-[-10px] w-[130px] h-[50px]">
-                            Back
-                          </Button>
                           <Button className="bg-gradient-to-r from-orange-600 to-orange-400 text-white shadow-lg rounded-lg text-xl scale-50 py-5 px-10 mb-[-10px] w-[130px] h-[50px]">
                             Next
                           </Button>
@@ -369,32 +196,28 @@ const Dashboard: React.FC = () => {
           }}
         >
           <p className="font-bold dark:text-white text-foodbg text-2xl pt-5 text-center">
-            Completed
+            Processing
           </p>
           <div className="width-[100%] mt-2 mx-4 !overflow-scroll h-[70vh] px-2">
             {itemCompletedQueue.map((_: any) => {
               return (
                 <div
                   className={`rounded-2xl px-2 py-2 mt-5`}
-                  style={{ boxShadow: `0 0 10px 0.1px ${colours[cCount]}` }}
+                  style={{ boxShadow: `0 0 10px 0.1px ${colours[cCount]} ` }}
                 >
                   <p className="text-center text-foodbg dark:text-white text-lg font-bold">
                     Order ID: {_.orderId}
                   </p>
                   <p className="text-center text-xs"> {_.time} min ago</p>
-                  {_.items.map((item: any) => {
+                  {_.foodItems.map((item: any) => {
                     return (
                       <div className="border border-foodbg rounded-xl py-2 px-2 flex items-center justify-between mt-5">
                         <div className="w-[60px] h-[60px] bg-green"></div>
                         <div className="w-[110px] h-[60px]">
                           <p className="text-md">{item.name}</p>
-                          <p className="text-sm">{item.description}</p>
                           <p className="text-xs">x{item.quentity}</p>
                         </div>
                         <div className="grid w-[100px]">
-                          <Button className="bg-gradient-to-r from-orange-600 to-orange-400 text-white shadow-lg rounded-lg text-xl scale-50 py-5 px-10 mb-[-10px] w-[130px] h-[50px]">
-                            Back
-                          </Button>
                           <Button className="bg-gradient-to-r from-orange-600 to-orange-400 text-white shadow-lg rounded-lg text-xl scale-50 py-5 px-10 mb-[-10px] w-[130px] h-[50px]">
                             Next
                           </Button>
