42f142c99b5a5669d4f0874fa37f3c0dbdd33346
iamtrazy <iamtrazy@proton.me>
2024-07-30 11:22:48 +0530

    fix: about us

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container } from '../../components/User/Container';
 import HeroImg from '../../images/Landing/Hero.png';
-import ChefImg from '../../images/Landing/chef.png';
+
 import ContactUs from '../../images/Landing/contact.png';
 import Gallery from '@/components/Landing/Gallery';
 import Menus from '@/components/Landing/Menus';
@@ -38,30 +38,21 @@ const HomePage: React.FC = () => {
         </div>
       </Container>
       <Container className="flex flex-wrap bg-transparent p-5">
-        <div className="flex items-center justify-center lg:w-1/2">
-          <img src={ChefImg} alt="Chef Image" width={500} height={500} />
-        </div>
-
         <div className="flex items-center lg:w-1/2">
-          <div className="max-w-2xl mx-15 my-25">
-            <h1 className="text-2xl py-4 font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight text-yellow-500">
-              What We Believe In
+          <div className="max-w-2xl mx-15 my-15 ">
+            <h1 className="text-2xl py-4 font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight text-white">
+              About Us
             </h1>
-            <p className="mb-2 py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl text-white">
-              Optimize your café operations with our comprehensive management
-              system, designed to enhance efficiency and elevate customer
-              satisfaction.
-            </p>
-
-            <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
-              <a
-                href="https://web3templates.com/templates/nextly-landing-page-template-for-startups"
-                target="_blank"
-                rel="noopener"
-                className="px-5 py-2.5 text-lg font-medium text-center text-white bg-yellow-500 rounded-md "
-              >
-                Explore menu
-              </a>
+            <div className="w-150">
+              <ReadMore
+                id="read-more-text"
+                text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae perspiciatis eligendi, similique quisquam esse aliquam possimus, 
+                              illum quaerat eaque illo dolor officiis. Temporibus odit, pariatur corporis ipsa odio officia tenetur? 
+                              Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae perspiciatis eligendi, similique quisquam esse aliquam possimus, 
+                              illum quaerat eaque illo dolor officiis. Temporibus odit, pariatur corporis ipsa odio officia tenetur? Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                              Beatae perspiciatis eligendi, similique quisquam esse aliquam possimus, illum quaerat eaque illo dolor officiis. Temporibus odit, pariatur corporis ipsa odio officia tenetur? 
+                              tetur adipisicing elit. Beatae perspiciatis eligendi, similique quisquam esse aliquam possimus, illum quaerat eaque illo dolor officiis"
+              />
             </div>
           </div>
         </div>
@@ -96,8 +87,8 @@ const HomePage: React.FC = () => {
         </div>
       </Container>
 
-      <Container >
-        <Menus/>
+      <Container>
+        <Menus />
       </Container>
 
       <Container>
