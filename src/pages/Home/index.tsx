ea3f90218e33336b190b7ff79880f2ab7799df6a
iamtrazy <iamtrazy@proton.me>
2024-07-30 13:31:25 +0530

    fix: About us

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Container } from '../../components/User/Container';
 import HeroImg from '../../images/Landing/Hero.png';
-
 import ContactUs from '../../images/Landing/contact.png';
+import chefImg from '@/images/Landing/chef.png';
 import Gallery from '@/components/Landing/Gallery';
 import Menus from '@/components/Landing/Menus';
 import { ReadMore } from '@/components/User/NavbarHome/readMore';
@@ -41,10 +41,10 @@ const HomePage: React.FC = () => {
       <Container className="flex flex-wrap bg-transparent p-5">
         <div className="flex items-center lg:w-1/2">
           <div className="max-w-2xl mx-15 my-15 ">
-            <h1 className="text-2xl py-4 font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight text-white">
+            <h1 className="text-2xl py-4 font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight text-yellow-500">
               About Us
             </h1>
-            <div className="w-150">
+            <div className="w-150 text-xl">
               <ReadMore
                 id="read-more-text"
                 text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae perspiciatis eligendi, similique quisquam esse aliquam possimus, 
@@ -57,34 +57,8 @@ const HomePage: React.FC = () => {
             </div>
           </div>
         </div>
-      </Container>
-
-      <Container className="flex flex-wrap bg-transparent p-10">
-        <div className="flex items-center lg:w-1/2">
-          <div className="max-w-2xl mx-15 my-25">
-            <h1 className="text-2xl py-4 font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight text-yellow-500">
-              Seamless Café Operations
-            </h1>
-            <p className="mb-2 py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl text-white">
-              Optimize your café operations with our comprehensive management
-              system, designed to enhance efficiency and elevate customer
-              satisfaction.
-            </p>
-
-            <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
-              <a
-                href="https://web3templates.com/templates/nextly-landing-page-template-for-startups"
-                target="_blank"
-                rel="noopener"
-                className="px-5 py-2.5 text-lg font-medium text-center text-white bg-yellow-500 rounded-md "
-              >
-                Explore menu
-              </a>
-            </div>
-          </div>
-        </div>
         <div className="flex items-center justify-center lg:w-1/2">
-          <img src={HeroImg} alt="Hero Image" width={425} height={425} />
+          <img src={chefImg} alt="Hero Image" width={425} height={425} />
         </div>
       </Container>
 
