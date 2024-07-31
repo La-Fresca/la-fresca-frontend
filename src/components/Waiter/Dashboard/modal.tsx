2ee3c5cfa7804ab5ba5e530e6c7dd928c42c2129
W.M.U.I.WANASINGHA <144322302+indusara1214@users.noreply.github.com>
2024-07-31 11:23:14 +0530

    fix: waiter

--- src/components/Waiter/Dashboard/modal.tsx
+++ src/components/Waiter/Dashboard/modal.tsx
@@ -15,14 +15,14 @@ const Modal: React.FC<ModalProps> = ({ order, onClose }) => {
         <div className="mb-2">
           <strong>Waiter:</strong> {order.waiter}
         </div>
-        <div className="mb-2">
+        <div className="mb-2 font-bold text-2xl text-yellow-500">
           <strong>Order:</strong> {order.order}
         </div>
         <div className="mb-2">
           <strong>Customer:</strong> {order.customer}
         </div>
         <div className="mb-2">
-          <strong>orderItems:</strong>dark coffee
+          <strong>orderItems:</strong> cheese pizza, pepperoni pizza, coke
         </div>
         <button
           className="bg-yellow-500 text-white px-4 py-2 rounded mt-4 transition transform hover:bg-gray-700 hover:-translate-y-1"
