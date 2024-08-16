e49eb80588f22dc4a0f4d9806d3b3bdab8df0f43
DasunThathsara <dasun.thathsara.sri@gmail.com>
2024-08-16 17:05:55 +0530

    feat: discount status of the food item and como cards

--- src/components/User/AllFoodItems/index.tsx
+++ src/components/User/AllFoodItems/index.tsx
@@ -56,36 +56,49 @@ function index() {
                     return (
                       <Link
                         to={`viewfood/${_.id}`}
-                        className="hover:scale-105 transition-transform duration-300 hover:cursor-pointer"
+                        className="hover:scale-105 transition-transform duration-300 hover:cursor-pointer mx-[10%] mt-[50px]"
                       >
-                        <Button
-                          className="bg-gradient-to-r from-orange-600 to-orange-400 text-white shadow-lg rounded-3xl min-w-5"
-                          style={{
-                            position: 'relative',
-                            left: '210px',
-                            top: '45px',
-                            zIndex: '1',
-                          }}
-                        >
-                          <b>+</b>
-                        </Button>
                         <div
-                          className="dark:border rounded-2xl border-foodbg bg-foodbg  backdrop-blur-md w-55 h-850 p-2 py-2"
+                          className="dark:border rounded-2xl border-foodbg bg-foodbg  backdrop-blur-md w-55 h-850 p-2 py-2 overflow-hidden"
                           style={{
-                            marginLeft: '10%',
-                            marginRight: '10%',
                             boxShadow: '0 0 10px 0 rgba(0, 0, 0, 0.2)',
                             backgroundColor: 'rgba(255, 255, 255, 0.01)',
                           }}
                         >
-                          <div className="h-50">
-                            <img src={_.image} alt="" className="w-[100%] h-[200px]" />
+                          <Button
+                            className="bg-gradient-to-r from-orange-600 to-orange-400 text-white shadow-lg rounded-full min-w-5"
+                            style={{
+                              position: 'absolute',
+                              right: '5px',
+                              top: '5px',
+                              zIndex: '1',
+                            }}
+                          >
+                            <b>+</b>
+                          </Button>
+
+                          {_.discountStatus == '1' && (
+                            <div className="absolute p-2 w-[120px] h-2 rotate-[135deg] bg-gradient-to-t from-red-600 to-red-900 translate-x-[-35px] translate-y-[20px]">
+                              <p className="rotate-180 text-xs translate-y-[-7.5px] translate-x-[-15px] text-white">
+                                PROMOTION
+                              </p>
+                            </div>
+                          )}
+
+                          <div className="h-50 rounded-xl">
+                            <img
+                              src={_.image}
+                              alt=""
+                              className="w-[100%] h-[200px] rounded-2xl"
+                            />
                           </div>
 
                           <div className="px-3 mt-2">
-                            <b className="text-foodbg dark:text-white text-xl">
-                              {_.name}
-                            </b>
+                            <div className="h-[50px]">
+                              <b className="text-foodbg dark:text-white text-xl">
+                                {_.name}
+                              </b>
+                            </div>
 
                             <div className="flex items-center pt-2">
                               {Array.from({ length: 5 }).map((j, i) => {
