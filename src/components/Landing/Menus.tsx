53257d89accdcfaf90ea5d9df4e6337d2e91e17c
Ravindu Hasanka <98080235+Ravindu-Hasanka@users.noreply.github.com>
2024-11-03 00:12:46 +0530

    Errors fixed

--- src/components/Landing/Menus.tsx
+++ src/components/Landing/Menus.tsx
@@ -6,7 +6,7 @@ import { TitleBox } from "./TitleBox";
 
 const Menus = () => {
     return (
-        <div className="dark:bg-black bg-white h-screen relative flex items-center justify-center">
+        <div className="dark:bg-black bg-white h-screen relative flex flex-col justify-center items-center">
             <img
                 src="https://images.unsplash.com/photo-1509042239860-f550ce710b93?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 style={{
@@ -21,12 +21,12 @@ const Menus = () => {
                 alt="Background"
             />
 
-            {/* Centered TitleBox for "find our Menus" */}
+            {/* TitleBox for "find our Menus" centered on small screens with 95% width */}
             <TitleBox
                 title="find our Menus"
                 subtitle="la Fresca cafe"
                 button="EXPLORE Now"
-                ClassName="absolute w-[95%] md:w-[50%] h-auto p-4 md:p-8 bg-yellow-500 text-center"
+                ClassName="absolute md:right-20 md:top-20 w-[95%] md:w-[50%] h-[40%] md:h-[50%] bg-yellow-500 p-4 md:p-8 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 md:translate-y-0 md:translate-x-0"
                 URLLink="/gallery"
             />
 
