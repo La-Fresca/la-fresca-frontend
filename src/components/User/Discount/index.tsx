65a340c29172f65fa1f4b4ebb5acadcfacbd77b7
S Manivannan <suruthi0611@gmail.com>
2024-11-02 14:30:01 +0530

    fix: Promotions Card

--- src/components/User/Discount/index.tsx
+++ src/components/User/Discount/index.tsx
@@ -83,7 +83,8 @@ function index() {
         <b>Promotions</b>
       </div>
       <div className="mt-2 mx-auto max-w-screen-xl px-4 2xl:px-0">
-        Enjoy great savings with exclusive deals and offers available on your favorite items.
+        Enjoy great savings with exclusive deals and offers available on your
+        favorite items.
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 mx-auto max-w-screen-xl mt-10">
@@ -94,7 +95,7 @@ function index() {
               className="hover:scale-105 transition-transform duration-300 hover:cursor-pointer"
             >
               <div
-                className="dark:border rounded-2xl border-foodbg bg-foodbg backdrop-blur-md xl:w-[500px] lg:w-[450px] h-[350px] p-2 py-2 mx-[10%] mb-10 overflow-hidden"
+                className="dark:border rounded-2xl border-foodbg bg-foodbg backdrop-blur-md xl:w-[500px] lg:w-[450px] h-[280px] p-2 py-2 mx-[10%] mb-10 overflow-hidden"
                 style={{
                   boxShadow: '0 0 10px 0 rgba(0, 0, 0, 0.12)',
                   backgroundColor: 'rgba(255, 255, 255, 0.01)',
@@ -112,22 +113,17 @@ function index() {
                   <img src={_.image} alt="" className="w-[100%]" />
                 </div>
 
-                <div className="px-3 mt-2">
-                  <p>
-                    <b className="dark:text-white text-foodbg text-xl">
-                      {_.name}
-                    </b>
-                  </p>
-                  <p>{_.branch}</p>
+                <div className="flex justify-between px-3 mt-2">
+                  <div className="">
+                    <p>
+                      <b className="dark:text-white text-foodbg text-xl">
+                        {_.name}
+                      </b>
+                    </p>
+                    <p>{_.branch}</p>
+                  </div>
 
-                  <hr className="mt-4 mb-3 border-foodbg" />
-
-                  <div className="flex justify-between">
-                    <div className="font-bold text-white pt-2 text-xl">
-                      <span className="pr-2 text-orange-500">Rs.</span>
-                      {_.price}
-                    </div>
-
+                  <div className="pt-[7px] pb-[3px]">
                     <TextButton value="Buy Now" />
                   </div>
                 </div>
