1e6681c3bb417747338fbba9b900cb9c75ae0b34
iamtrazy <iamtrazy@proton.me>
2024-11-30 16:35:22 +0530

    feat: Discount list backend connected

--- src/components/BranchManager/Discount/Adddiscount.tsx
+++ src/components/BranchManager/Discount/Adddiscount.tsx
@@ -1,189 +1,221 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from './Button';
+import { z } from 'zod';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useDiscount } from '@/api/useDiscount';
+import { Discount } from '@/types/discount';
+import { swalSuccess } from '@/components/UI/SwalSuccess';
 
-interface Discount {
-  description: string;
-  name: string;
-  menuItemType: string;
-  menuItemId: string;
-  discountAmount: number;
-  discountType: string;
-  amount: number;
-  offerDetails: string;
-  startDate: string;
-  endDate: string;
-}
+const FormSchema = z.object({
+  name: z.string().min(1, { message: 'Discount Name is required' }),
+  menuItemType: z.string().min(1, { message: 'Menu Item Type is required' }),
+  menuItemId: z.string().min(1, { message: 'Menu Item ID is required' }),
+  discountAmount: z.coerce
+    .number()
+    .multipleOf(0.01)
+    .min(0, { message: 'Discount amount must be at least 0' }),
+  amount: z.coerce
+    .number()
+    .multipleOf(0.01)
+    .min(0, { message: 'Amount must be at least 0' }),
+  // Add other fields here
+  discountType: z.enum(['Percentage', 'Fixed'], {
+    required_error: 'Discount Type is required',
+  }),
+  offerDetails: z.string().min(1, { message: 'Offer Details is required' }),
+  startDate: z.string().min(1, { message: 'Start Date is required' }),
+  endDate: z.string().min(1, { message: 'End Date is required' }),
+});
+
+type FormSchemaType = z.infer<typeof FormSchema>;
 
 const AddDiscount: React.FC = () => {
-  const [discount, setDiscount] = useState<Discount>({
-    description: '',
-    name: '',
-    menuItemType: '',
-    menuItemId: '',
-    discountAmount: 0,
-    discountType: '',
-    amount: 0,
-    offerDetails: '',
-    startDate: '',
-    endDate: ''
-  });
-
   const navigate = useNavigate();
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
-    const { name, value } = e.target;
+  const { addDiscount } = useDiscount();
+  const { showSwal } = swalSuccess({
+    message: 'Discount Added successfully',
+  });
 
-    // Prevent negative values for discountAmount and amount
-    if ((name === 'discountAmount' || name === 'amount') && parseFloat(value) < 0) {
-      return; // Prevent updating with a negative value
+  const { register, handleSubmit, formState } = useForm<FormSchemaType>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  const onSubmit: SubmitHandler<FormSchemaType> = async (data) => {
+    try {
+      addDiscount(data);
+    } catch (error) {
+      console.error('Error adding discount', error);
+    } finally {
+      setTimeout(() => {
+        showSwal();
+        navigate('/branch-manager/discounts');
+      }, 2000);
     }
-
-    setDiscount((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
   };
 
-  const handleSaveDiscount = () => {
-    // Check for negative values before saving
-    if (discount.discountAmount < 0 || discount.amount < 0) {
-      alert("Discount Amount and Amount cannot be negative.");
-      return;
-    }
-
-    // Logic to save the discount (e.g., API call or state update)
-    console.log('Discount saved:', discount);
-
-    // Navigate back to the discount list page (or wherever you want after saving)
-    navigate('/discounts');
-  };
+  const { errors } = formState;
+  console.log(errors);
 
   return (
     <div className="min-h-screen bg-gray-900 text-white p-4">
-      <div className="container mx-auto">
-        <h2 className="text-2xl font-bold mb-4">Add Discount</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="container mx-auto">
+          <h2 className="text-2xl font-bold mb-4">Add Discount</h2>
 
-        <div className="grid gap-4 md:grid-cols-2">
-          <div>
-            <label htmlFor="name" className="block text-sm font-medium text-gray-300">Discount Name</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={discount.name}
-              onChange={handleChange}
-              className="w-full p-2 mt-1  text-white bg-gray border border-gray-500 rounded focus:outline-none focus:ring-2 focus:ring-yellow-500"
-            />
+          <div className="grid gap-4 md:grid-cols-2">
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-gray-300"
+              >
+                Discount Name
+              </label>
+              <input
+                type="text"
+                className="w-full p-2 mt-1  text-white bg-gray border border-gray-500 rounded focus:outline-none focus:ring-2 focus:ring-yellow-500"
+                {...register('name')}
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="menuItemType"
+                className="block text-sm font-medium text-gray-300"
+              >
+                Menu Item Type
+              </label>
+              <input
+                type="text"
+                className="w-full p-2 mt-1  text-white bg-gray border border-gray-500 rounded focus:outline-none focus:ring-2 focus:ring-yellow-500"
+                {...register('menuItemType')}
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="menuItemId"
+                className="block text-sm font-medium text-gray-300"
+              >
+                Menu Item ID
+              </label>
+              <input
+                type="text"
+                id="menuItemId"
+                className="w-full p-2 mt-1  text-white bg-gray border border-gray-500 rounded focus:outline-none focus:ring-2 focus:ring-yellow-500"
+                {...register('menuItemId')}
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="discountAmount"
+                className="block text-sm font-medium text-gray-300"
+              >
+                Discount Amount (LKR)
+              </label>
+              <input
+                type="number"
+                id="discountAmount"
+                className="w-full p-2 mt-1 text-white bg-gray border border-gray-500 rounded focus:outline-none focus:ring-2 focus:ring-yellow-500"
+                {...register('discountAmount')}
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="discountType"
+                className="block text-sm font-medium text-gray-300"
+              >
+                Discount Type
+              </label>
+              <select
+                className="w-full p-2 mt-1  text-white bg-gray border border-gray-500 rounded focus:outline-none focus:ring-2 focus:ring-yellow-500"
+                {...register('discountType')}
+              >
+                <option value="Percentage">Percentage</option>
+                <option value="Fixed">Fixed</option>
+              </select>
+            </div>
+
+            <div>
+              <label
+                htmlFor="amount"
+                className="block text-sm font-medium text-gray-300"
+              >
+                Amount (LKR)
+              </label>
+              <input
+                type="number"
+                id="amount"
+                className="w-full p-2 mt-1  text-white bg-gray border border-gray-500 rounded focus:outline-none focus:ring-2 focus:ring-yellow-500"
+                {...register('amount')}
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="offerDetails"
+                className="block text-sm font-medium text-gray-300"
+              >
+                Offer Details
+              </label>
+              <textarea
+                id="offerDetails"
+                className="w-full p-2 mt-1  text-white bg-gray border border-gray-500 rounded focus:outline-none focus:ring-2 focus:ring-yellow-500"
+                {...register('offerDetails')}
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="startDate"
+                className="block text-sm font-medium text-gray-300"
+              >
+                Start Date
+              </label>
+              <input
+                type="datetime-local"
+                id="startDate"
+                className="w-full p-2 mt-1  text-white bg-gray border border-gray-500 rounded focus:outline-none focus:ring-2 focus:ring-yellow-500"
+                {...register('startDate')}
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="endDate"
+                className="block text-sm font-medium text-gray-300"
+              >
+                End Date
+              </label>
+              <input
+                type="datetime-local"
+                id="endDate"
+                className="w-full p-2 mt-1  text-white bg-gray border border-gray-500 rounded focus:outline-none focus:ring-2 focus:ring-yellow-500"
+                {...register('endDate')}
+              />
+            </div>
           </div>
-
-          <div>
-            <label htmlFor="menuItemType" className="block text-sm font-medium text-gray-300">Menu Item Type</label>
-            <input
-              type="text"
-              id="menuItemType"
-              name="menuItemType"
-              value={discount.menuItemType}
-              onChange={handleChange}
-              className="w-full p-2 mt-1  text-white bg-gray border border-gray-500 rounded focus:outline-none focus:ring-2 focus:ring-yellow-500"
-            />
-          </div>
-
-          <div>
-            <label htmlFor="menuItemId" className="block text-sm font-medium text-gray-300">Menu Item ID</label>
-            <input
-              type="text"
-              id="menuItemId"
-              name="menuItemId"
-              value={discount.menuItemId}
-              onChange={handleChange}
-              className="w-full p-2 mt-1  text-white bg-gray border border-gray-500 rounded focus:outline-none focus:ring-2 focus:ring-yellow-500"
-            />
-          </div>
-
-          <div>
-            <label htmlFor="discountAmount" className="block text-sm font-medium text-gray-300">Discount Amount (LKR)</label>
-            <input
-              type="number"
-              id="discountAmount"
-              name="discountAmount"
-              value={discount.discountAmount}
-              onChange={handleChange}
-              className="w-full p-2 mt-1 text-white bg-gray border border-gray-500 rounded focus:outline-none focus:ring-2 focus:ring-yellow-500"
-            />
-          </div>
-
-          <div>
-            <label htmlFor="discountType" className="block text-sm font-medium text-gray-300">Discount Type</label>
-            <select
-              name="discountType"
-              value={discount.discountType}
-              onChange={handleChange}
-              className="w-full p-2 mt-1  text-white bg-gray border border-gray-500 rounded focus:outline-none focus:ring-2 focus:ring-yellow-500"
+          <div className="flex items-center justify-end gap-4 mt-6">
+            <button
+              type="button"
+              onClick={() => navigate('/branch-manager/discounts')}
+              className="bg-transparent hover:bg-transparent text-white px-8 py-4 border-2 rounded-lg transition duration-200"
             >
-              <option value="Percentage">Percentage</option>
-              <option value="Fixed">Fixed</option>
-            </select>
-          </div>
-
-          <div>
-            <label htmlFor="amount" className="block text-sm font-medium text-gray-300">Amount (LKR)</label>
-            <input
-              type="number"
-              id="amount"
-              name="amount"
-              value={discount.amount}
-              onChange={handleChange}
-              className="w-full p-2 mt-1  text-white bg-gray border border-gray-500 rounded focus:outline-none focus:ring-2 focus:ring-yellow-500"
-            />
-          </div>
-
-          <div>
-            <label htmlFor="offerDetails" className="block text-sm font-medium text-gray-300">Offer Details</label>
-            <textarea
-              id="offerDetails"
-              name="offerDetails"
-              value={discount.offerDetails}
-              onChange={handleChange}
-              className="w-full p-2 mt-1  text-white bg-gray border border-gray-500 rounded focus:outline-none focus:ring-2 focus:ring-yellow-500"
-            />
-          </div>
-
-          <div>
-            <label htmlFor="startDate" className="block text-sm font-medium text-gray-300">Start Date</label>
-            <input
-              type="datetime-local"
-              id="startDate"
-              name="startDate"
-              value={discount.startDate}
-              onChange={handleChange}
-              className="w-full p-2 mt-1  text-white bg-gray border border-gray-500 rounded focus:outline-none focus:ring-2 focus:ring-yellow-500"
-            />
-          </div>
-
-          <div>
-            <label htmlFor="endDate" className="block text-sm font-medium text-gray-300">End Date</label>
-            <input
-              type="datetime-local"
-              id="endDate"
-              name="endDate"
-              value={discount.endDate}
-              onChange={handleChange}
-              className="w-full p-2 mt-1  text-white bg-gray border border-gray-500 rounded focus:outline-none focus:ring-2 focus:ring-yellow-500"
-            />
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="bg-yellow-500 hover:bg-yellow-700 text-white px-4 py-2 rounded transition duration-300"
+            >
+              Save
+            </button>
           </div>
         </div>
-        <div className="flex items-center justify-end gap-4 mt-6">
-          <button
-            type="button"
-            onClick={() => navigate('/branch-manager/discounts')}
-            className="bg-transparent hover:bg-transparent text-white px-8 py-4 border-2 rounded-lg transition duration-200"
-          >
-            Cancel
-          </button>
-          <Button text="Save Discount" onClick={handleSaveDiscount} />
-        </div>
-      </div>
+      </form>
     </div>
   );
 };
--- src/components/BranchManager/Discount/DiscountTable.tsx
+++ src/components/BranchManager/Discount/DiscountTable.tsx
@@ -1,85 +1,75 @@
 import React, { useState } from 'react';
-
-interface Discount {
-  description: string;
-  name: string;
-  menuItemType: string;
-  menuItemId: string;
-  discountAmount: number;
-  discountType: string;
-  amount: number;
-  offerDetails: string;
-  startDate: string;
-  endDate: string;
-}
+import { Discount } from '@/types/discount';
+import { useDiscount } from '@/api/useDiscount';
+import { useQuery } from '@tanstack/react-query';
 
 interface DiscountTableProps {
   filterStatus: string;
   toggleEditModal: (discount: Discount | null) => void;
 }
 
-const DiscountTable: React.FC<DiscountTableProps> = ({ filterStatus, toggleEditModal }) => {
-  // Sample data array stored in DiscountTable component
-  const [discounts] = useState<Discount[]>([
-    {
-      description: '10% off on main course',
-      name: 'Main Course Discount',
-      menuItemType: 'Main Course',
-      menuItemId: 'MC001',
-      discountAmount: 10,
-      discountType: 'Percentage',
-      amount: 0,  // Update this if necessary
-      offerDetails: 'Applicable to all main course items',
-      startDate: '2024-11-01',
-      endDate: '2024-12-01',
-    },
-    {
-      description: '20% off on beverages',
-      name: 'Beverage Discount',
-      menuItemType: 'Beverages',
-      menuItemId: 'BV002',
-      discountAmount: 20,
-      discountType: 'Percentage',
-      amount: 0,  // Update this if necessary
-      offerDetails: 'Applicable to all beverages',
-      startDate: '2024-11-05',
-      endDate: '2024-12-05',
-    },
-    // Add more discounts as needed
-  ]);
+const DiscountTable: React.FC<DiscountTableProps> = ({
+  filterStatus,
+  toggleEditModal,
+}) => {
+  const { getAllDiscounts } = useDiscount();
+
+  const discountQuery = useQuery({
+    queryKey: ['discount'],
+    queryFn: getAllDiscounts,
+  });
+
+  if (discountQuery.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  const discounts: Discount[] = discountQuery.data;
+  console.log(discounts);
 
   return (
     <div className="bg-transparent text-white shadow overflow-hidden sm:rounded-lg">
       <table className="min-w-full divide-y divide-gray-700">
         <thead className="bg-transparent">
           <tr className="font-noto-serif">
-            <th className="px-2 py-3 text-left text-sm font-bold text-white uppercase tracking-wider">Name</th>
-            <th className="px-2 py-3 text-left text-sm font-bold text-white uppercase tracking-wider">Menu Item</th>
-            <th className="px-2 py-3 text-left text-sm font-bold text-white uppercase tracking-wider">Discount Amount</th>
-            <th className="px-2 py-3 text-left text-sm font-bold text-white uppercase tracking-wider">Discount Type</th>
-            <th className="px-2 py-3 text-left text-sm font-bold text-white uppercase tracking-wider">Start Date</th>
-            <th className="px-2 py-3 text-left text-sm font-bold text-white uppercase tracking-wider">End Date</th>
+            <th className="px-2 py-3 text-left text-sm font-bold text-white uppercase tracking-wider">
+              Name
+            </th>
+            <th className="px-2 py-3 text-left text-sm font-bold text-white uppercase tracking-wider">
+              Menu Item
+            </th>
+            <th className="px-2 py-3 text-left text-sm font-bold text-white uppercase tracking-wider">
+              Discount Amount
+            </th>
+            <th className="px-2 py-3 text-left text-sm font-bold text-white uppercase tracking-wider">
+              Discount Type
+            </th>
+            <th className="px-2 py-3 text-left text-sm font-bold text-white uppercase tracking-wider">
+              Start Date
+            </th>
+            <th className="px-2 py-3 text-left text-sm font-bold text-white uppercase tracking-wider">
+              End Date
+            </th>
             <th className="px-2 py-3"></th>
           </tr>
         </thead>
         <tbody className="bg-gray-800 divide-y divide-gray-700">
           {discounts
-            .filter(discount => filterStatus === 'All' || discount.discountType === filterStatus)
+            .filter(
+              (discount) =>
+                filterStatus === 'All' ||
+                discount[0].discountType === filterStatus,
+            )
             .map((discount) => (
-              <tr key={discount.menuItemId}>
-                <td className="px-2 py-2">{discount.name}</td>
-                <td className="px-2 py-2">{discount.menuItemType}</td>
-                <td className="px-2 py-2">LKR {discount.discountAmount}</td>
-                <td className="px-2 py-2">{discount.discountType}</td>
-                <td className="px-2 py-2">{new Date(discount.startDate).toLocaleDateString()}</td>
-                <td className="px-2 py-2">{new Date(discount.endDate).toLocaleDateString()}</td>
+              <tr key={discount[0].menuItemId}>
+                <td className="px-2 py-2">{discount[0].name}</td>
+                <td className="px-2 py-2">{discount[0].menuItemType}</td>
+                <td className="px-2 py-2">LKR {discount[0].discountAmount}</td>
+                <td className="px-2 py-2">{discount[0].discountType}</td>
                 <td className="px-2 py-2">
-                  <button
-                    onClick={() => toggleEditModal(discount)}
-                    className="bg-yellow-600 hover:bg-orange-700 text-white px-4 py-2 rounded transition duration-300"
-                  >
-                    Edit
-                  </button>
+                  {new Date(discount[0].startDate).toLocaleDateString()}
+                </td>
+                <td className="px-2 py-2">
+                  {new Date(discount[0].endDate).toLocaleDateString()}
                 </td>
               </tr>
             ))}
