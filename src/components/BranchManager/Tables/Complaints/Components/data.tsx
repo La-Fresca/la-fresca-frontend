175e2a3fa3f7e9e4f347d58deccbb5fb62db196a
DasunThathsara <dasun.thathsara.sri@gmail.com>
2024-12-01 23:15:43 +0530

    fix: view complaints - branch manager

--- src/components/BranchManager/Tables/Complaints/Components/data.tsx
+++ src/components/BranchManager/Tables/Complaints/Components/data.tsx
@@ -1,14 +1,7 @@
 const columns = [
-  {name: "Title", uid: "name", sortable: true},
-  {name: "Description", uid: "price", sortable: true},
-  {name: "Date", uid: "available", sortable: true},
-  {name: "Status", uid: "status", sortable: true}
+  {name: "Title", uid: "topic", sortable: true},
+  {name: "Description", uid: "description", sortable: true},
+  {name: "Date", uid: "date", sortable: true}
 ];
 
-const statusOptions = [
-  {name: "Unread", uid: '0'},
-  {name: "Read", uid: '1'},
-  {name: "Resolved", uid: '2'},
-];
-
-export {columns, statusOptions};
+export {columns};
---
+++ src/types/complaint.ts
@@ -0,0 +1,9 @@
+export type Complaint = {
+    id?: string;
+    name: string;
+    description?: string;
+    date: string;
+    topic: string;
+    complainer: string;
+    complainee: string;
+  };
