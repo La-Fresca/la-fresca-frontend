9cef810071ea48a9ddcc1fd1bade4965de2fc144
W.M.U.I.WANASINGHA <144322302+indusara1214@users.noreply.github.com>
2024-12-01 07:33:16 +0530

    usermanagement fixed

--- src/components/BranchManager/UserManagement/AddUser.tsx
+++ src/components/BranchManager/UserManagement/AddUser.tsx
@@ -57,121 +57,131 @@ const AddUser: React.FC = () => {
     <div className="min-h-screen bg-transparent text-white p-8">
       <h2 className="text-4xl mb-4 font-bold">Add User</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-4">
-          <label className="block mb-2">First Name</label>
-          <input
-            type="text"
-            className="w-2/3 p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
-            {...register('firstName')}
-          />
-          {errors.firstName && (
-            <p className="text-red-500 text-xs mt-1">
-              {errors.firstName.message}
-            </p>
-          )}
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+          <div className="mb-2">
+            <label className="block mb-2">First Name</label>
+            <input
+              type="text"
+              placeholder='First Name'
+              className="w-full p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
+              {...register('firstName')}
+            />
+            {errors.firstName && (
+              <p className="text-red-500 text-xs mt-1">
+                {errors.firstName.message}
+              </p>
+            )}
+          </div>
+          <div className="mb-2">
+            <label className="block mb-2">Last Name</label>
+            <input
+              type="text"
+              placeholder='Last Name'
+              className="w-full p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
+              {...register('lastName')}
+            />
+            {errors.lastName && (
+              <p className="text-red-500 text-xs mt-1">
+                {errors.lastName.message}
+              </p>
+            )}
+          </div>
+          <div className="mb-2">
+            <label className="block mb-2">Username</label>
+            <input
+              type="text"
+              placeholder='Username'
+              className="w-full p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
+              {...register('username')}
+            />
+            {errors.username && (
+              <p className="text-red-500 text-xs mt-1">
+                {errors.username.message}
+              </p>
+            )}
+          </div>
+          <div className="mb-2">
+            <label className="block mb-2">Email</label>
+            <input
+              type="email"
+              placeholder='abc@gmail.com'
+              className="w-full p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
+              {...register('email')}
+            />
+            {errors.email && (
+              <p className="text-red-500 text-xs mt-1">{errors.email.message}</p>
+            )}
+          </div>
+          <div className="mb-2">
+            <label className="block mb-2">Address</label>
+            <input
+              type="text"
+              placeholder='Address'
+              className="w-full p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
+              {...register('address')}
+            />
+            {errors.address && (
+              <p className="text-red-500 text-xs mt-1">
+                {errors.address.message}
+              </p>
+            )}
+          </div>
+          <div className="mb-2">
+            <label className="block mb-2">Phone</label>
+            <input
+              type="text"
+              placeholder='Eg: 1234567890'
+              className="w-full p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
+              {...register('phoneNumber')}
+            />
+            {errors.phoneNumber && (
+              <p className="text-red-500 text-xs mt-1">
+                {errors.phoneNumber.message}
+              </p>
+            )}
+          </div>
+          <div className="mb-2">
+            <label className="block mb-2">Group</label>
+            <select
+              className="w-full p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
+              {...register('role')}
+            >
+              <option value="WAITER">Waiter</option>
+              <option value="KITCHEN MANAGER">Kitchen Manager</option>
+              <option value="STOREKEEPER">Store Keeper</option>
+              <option value="CASHIER">Cashier</option>
+            </select>
+          </div>
+          <div className="mb-2">
+            <label className="block mb-2">Password</label>
+            <input
+              type="password"
+              placeholder='Type strong password'
+              className="w-full p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
+              {...register('password')}
+            />
+            {errors.password && (
+              <p className="text-red-500 text-xs mt-1">
+                {errors.password.message}
+              </p>
+            )}
+          </div>
+          <div className="mb-2">
+            <label className="block mb-2">Confirm Password</label>
+            <input
+              type="password"
+              placeholder='Confirm Password'
+              className="w-full p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
+              {...register('confirmPassword')}
+            />
+            {errors.confirmPassword && (
+              <p className="text-red-500 text-xs mt-1">
+                {errors.confirmPassword.message}
+              </p>
+            )}
+          </div>
         </div>
-        <div className="mb-4">
-          <label className="block mb-2">Last Name</label>
-          <input
-            type="text"
-            className="w-2/3 p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
-            {...register('lastName')}
-          />
-          {errors.lastName && (
-            <p className="text-red-500 text-xs mt-1">
-              {errors.lastName.message}
-            </p>
-          )}
-        </div>
-        <div className="mb-4">
-          <label className="block mb-2">Name</label>
-          <input
-            type="text"
-            className="w-2/3 p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
-            {...register('username')}
-          />
-          {errors.username && (
-            <p className="text-red-500 text-xs mt-1">
-              {errors.username.message}
-            </p>
-          )}
-        </div>
-        <div className="mb-4">
-          <label className="block mb-2">Email</label>
-          <input
-            type="email"
-            className="w-2/3 p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
-            {...register('email')}
-          />
-          {errors.email && (
-            <p className="text-red-500 text-xs mt-1">{errors.email.message}</p>
-          )}
-        </div>
-        <div className="mb-4">
-          <label className="block mb-2">Address</label>
-          <input
-            type="text"
-            className="w-2/3 p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
-            {...register('address')}
-          />
-          {errors.address && (
-            <p className="text-red-500 text-xs mt-1">
-              {errors.address.message}
-            </p>
-          )}
-        </div>
-        <div className="mb-4">
-          <label className="block mb-2">Phone</label>
-          <input
-            type="text"
-            className="w-2/3 p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
-            {...register('phoneNumber')}
-          />
-          {errors.phoneNumber && (
-            <p className="text-red-500 text-xs mt-1">
-              {errors.phoneNumber.message}
-            </p>
-          )}
-        </div>
-        <div className="mb-4">
-          <label className="block mb-2">Group</label>
-          <select
-            className="w-2/3 p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
-            {...register('role')}
-          >
-            <option value="WAITER">Waiter</option>
-            <option value="KITCHEN MANAGER">Kitchen Manager</option>
-            <option value="STOREKEEPER">Store Keeper</option>
-            <option value="CASHIER">Cashier</option>
-          </select>
-        </div>
-        <div className="mb-4">
-          <label className="block mb-2">Password</label>
-          <input
-            type="password"
-            className="w-2/3 p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
-            {...register('password')}
-          />
-          {errors.password && (
-            <p className="text-red-500 text-xs mt-1">
-              {errors.password.message}
-            </p>
-          )}
-        </div>
-        <div className="mb-4">
-          <label className="block mb-2">Confirm Password</label>
-          <input
-            type="password"
-            className="w-2/3 p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
-            {...register('confirmPassword')}
-          />
-          {errors.confirmPassword && (
-            <p className="text-red-500 text-xs mt-1">
-              {errors.confirmPassword.message}
-            </p>
-          )}
-        </div>
-        <div className="flex items-center mt-15">
+        <div className="flex items-center justify-end mt-2 gap-4">
           <button
             type="button"
             onClick={() => navigate('/branch-manager/users')}
@@ -181,7 +191,7 @@ const AddUser: React.FC = () => {
           </button>
           <button
             type="submit"
-            className="bg-yellow-500 hover:bg-yellow-700 text-white px-4 py-2 rounded transition duration-300"
+            className="text-white bg-gradient-to-r from-orange-600 to-orange-400 hover:from-orange-400 hover:to-orange-600 px-12 py-4 rounded-lg transition duration-300 shadow-md"
           >
             Save
           </button>
--- src/components/BranchManager/UserManagement/EditUser.tsx
+++ src/components/BranchManager/UserManagement/EditUser.tsx
@@ -21,7 +21,6 @@ type FormSchemaType = z.infer<typeof FormSchema>;
 
 const EditUser: React.FC = () => {
   const { userId } = useParams<{ userId: string }>();
-  console.log(userId);
   const [Loading, setLoading] = useState<boolean>(true);
   const [user, setUser] = useState<User>();
   const navigate = useNavigate();
@@ -36,7 +35,6 @@ const EditUser: React.FC = () => {
   });
 
   const { errors } = formState;
-  console.log(errors);
 
   const onSubmit: SubmitHandler<FormSchemaType> = async (data) => {
     const transformedData: User = {
@@ -61,7 +59,6 @@ const EditUser: React.FC = () => {
       const user = await getUserById(userId);
       setUser(user);
       setLoading(false);
-      console.log(user);
     } catch (error) {
       console.error(error);
     }
@@ -76,97 +73,99 @@ const EditUser: React.FC = () => {
   }
 
   return (
-    <div className="min-h-screen bg-transparent text-white p-8 ">
-      <h2 className="text-xl mb-4 font-bold">Edit User</h2>
+    <div className="min-h-screen bg-transparent text-white p-8">
+      <h2 className="text-4xl mb-4 font-bold">Edit User</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-4">
-          <label className="block mb-2">First Name</label>
-          <input
-            type="text"
-            value={user?.firstName}
-            className="w-2/3 p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
-            {...register('firstName')}
-            onChange={(e) => setUser({ ...user, firstName: e.target.value })}
-          />
-          {errors.firstName && (
-            <p className="text-red-500 text-xs mt-1">
-              {errors.firstName.message}
-            </p>
-          )}
-        </div>
-        <div className="mb-4">
-          <label className="block mb-2">Last Name</label>
-          <input
-            type="text"
-            value={user?.lastName}
-            className="w-2/3 p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
-            {...register('lastName')}
-            onChange={(e) => setUser({ ...user, lastName: e.target.value })}
-          />
-          {errors.lastName && (
-            <p className="text-red-500 text-xs mt-1">
-              {errors.lastName.message}
-            </p>
-          )}
-        </div>
-        <div className="mb-4">
-          <label className="block mb-2">Email</label>
-          <input
-            type="email"
-            value={user?.email}
-            className="w-2/3 p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
-            {...register('email')}
-            onChange={(e) => setUser({ ...user, lastName: e.target.value })}
-          />
-          {errors.email && (
-            <p className="text-red-500 text-xs mt-1">{errors.email.message}</p>
-          )}
-        </div>
-        <div className="mb-4">
-          <label className="block mb-2">Status</label>
-          <select
-            value={user?.status}
-            className="w-2/3 p-2 border border-gray-500 rounded bg-gray-800 text-white bg-black focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
-            {...register('status')}
-            onChange={(e) => setUser({ ...user, status: e.target.value })}
-          >
-            <option value="Active">Active</option>
-            <option value="Inactive">Inactive</option>
-          </select>
-        </div>
-        <div className="mb-4">
-          <label className="block mb-2">Location</label>
-          <input
-            type="text"
-            value={user?.address}
-            className="w-2/3 p-2 border border-gray-500 rounded bg-gray-800 text-white bg-black focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
-            {...register('address')}
-            onChange={(e) => setUser({ ...user, address: e.target.value })}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block mb-2">Phone</label>
-          <input
-            type="text"
-            value={user?.phoneNumber}
-            className="w-2/3 p-2 border border-gray-500 rounded bg-gray-800 text-white bg-black focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
-            {...register('phoneNumber')}
-            onChange={(e) => setUser({ ...user, phoneNumber: e.target.value })}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block mb-2">Group</label>
-          <select
-            className="w-2/3 p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
-            {...register('role')}
-            defaultValue={user?.role}
-            onChange={(e) => setUser({ ...user, role: e.target.value })}
-          >
-            <option value="WAITER">Waiter</option>
-            <option value="KITCHEN MANAGER">Kitchen Manager</option>
-            <option value="STOREKEEPER">Store Keeper</option>
-            <option value="CASHIER">Cashier</option>
-          </select>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+          <div className="mb-4">
+            <label className="block mb-2">First Name</label>
+            <input
+              type="text"
+              value={user?.firstName}
+              className="w-full p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
+              {...register('firstName')}
+              onChange={(e) => setUser({ ...user, firstName: e.target.value })}
+            />
+            {errors.firstName && (
+              <p className="text-red-500 text-xs mt-1">
+                {errors.firstName.message}
+              </p>
+            )}
+          </div>
+          <div className="mb-4">
+            <label className="block mb-2">Last Name</label>
+            <input
+              type="text"
+              value={user?.lastName}
+              className="w-full p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
+              {...register('lastName')}
+              onChange={(e) => setUser({ ...user, lastName: e.target.value })}
+            />
+            {errors.lastName && (
+              <p className="text-red-500 text-xs mt-1">
+                {errors.lastName.message}
+              </p>
+            )}
+          </div>
+          <div className="mb-4">
+            <label className="block mb-2">Email</label>
+            <input
+              type="email"
+              value={user?.email}
+              className="w-full p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
+              {...register('email')}
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
+            />
+            {errors.email && (
+              <p className="text-red-500 text-xs mt-1">{errors.email.message}</p>
+            )}
+          </div>
+          <div className="mb-4">
+            <label className="block mb-2">Status</label>
+            <select
+              value={user?.status}
+              className="w-full p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
+              {...register('status')}
+              onChange={(e) => setUser({ ...user, status: e.target.value })}
+            >
+              <option value="Active">Active</option>
+              <option value="Inactive">Inactive</option>
+            </select>
+          </div>
+          <div className="mb-4">
+            <label className="block mb-2">Location</label>
+            <input
+              type="text"
+              value={user?.address}
+              className="w-full p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
+              {...register('address')}
+              onChange={(e) => setUser({ ...user, address: e.target.value })}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block mb-2">Phone</label>
+            <input
+              type="text"
+              value={user?.phoneNumber}
+              className="w-full p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
+              {...register('phoneNumber')}
+              onChange={(e) => setUser({ ...user, phoneNumber: e.target.value })}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block mb-2">Group</label>
+            <select
+              className="w-full p-2 border border-gray-500 rounded bg-black text-white focus:outline-none focus:ring-2 focus:ring-orange-600 transition duration-300"
+              {...register('role')}
+              defaultValue={user?.role}
+              onChange={(e) => setUser({ ...user, role: e.target.value })}
+            >
+              <option value="WAITER">Waiter</option>
+              <option value="KITCHEN MANAGER">Kitchen Manager</option>
+              <option value="STOREKEEPER">Store Keeper</option>
+              <option value="CASHIER">Cashier</option>
+            </select>
+          </div>
         </div>
         <div className="flex items-center justify-end mt-4 gap-4">
           <button
@@ -178,7 +177,7 @@ const EditUser: React.FC = () => {
           </button>
           <button
             type="submit"
-            className="bg-yellow-500 hover:bg-yellow-700 text-black px-4 py-2 rounded transition duration-300"
+            className="text-white bg-gradient-to-r from-orange-600 to-orange-400 hover:from-orange-400 hover:to-orange-600 px-12 py-4 rounded-lg transition duration-300 shadow-md"
           >
             Save
           </button>
