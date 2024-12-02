b33ab69b911f9d922f9e1f4ba25df4dcece42e18
S Manivannan <suruthi0611@gmail.com>
2024-12-02 13:01:15 +0530

    fix: discount data file

--- src/components/TopLevelManager/Tables/Discounts/Components/data.tsx
+++ src/components/TopLevelManager/Tables/Discounts/Components/data.tsx
@@ -1,17 +1,19 @@
 const columns = [
-  {name: "ID", uid: "id", sortable: true},
   {name: "Name", uid: "name", sortable: true},
-  {name: "Price", uid: "price", sortable: true},
-  {name: "Availability", uid: "available", sortable: true},
-  {name: "Discount", uid: "discountStatus"},
-  {name: "Features", uid: "features", sortable: true},
-  {name: "Status", uid: "status", sortable: true},
-  {name: "Actions", uid: "actions"}
+  {name: "Description", uid: "description", sortable: true},
+  {name: "Discount Type", uid: "discountType", sortable: true},
+  {name: "Menu Item Type", uid: "menuItemType"},
+  {name: "Amount", uid: "amount", sortable: true},
+  {name: "Discount Amount", uid: "discountAmount", sortable: true},
+  {name: "Start Date", uid: "startDate", sortable: true},
+  {name: "End Date", uid: "endDate", sortable: true},
+  {name: "Offer Details", uid: "offerDetails", sortable: true},
+  {name: "Status", uid: "status", sortable: true}
 ];
 
 const statusOptions = [
-  {name: "Approved", uid: '0'},
-  {name: "Pending", uid: '2'},
+  {name: "Approved", uid: 0},
+  {name: "Pending", uid: 2},
 ];
 
 export {columns, statusOptions};
