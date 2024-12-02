dc86806a423af7f51e8f343c79fa90e998012019
iamtrazy <iamtrazy@proton.me>
2024-12-02 11:27:57 +0530

    discount table styles

--- src/pages/BranchManager/Discounts/index.tsx
+++ src/pages/BranchManager/Discounts/index.tsx
@@ -19,10 +19,6 @@ const DiscountManagement: React.FC = () => {
   const [filterStatus, setFilterStatus] = useState('All');
   const navigate = useNavigate();
 
-  const handleAddDiscount = () => {
-    navigate('add');
-  };
-
   const handleEditDiscount = (discount: Discount | null) => {
     if (discount) {
       navigate(`edit/${discount.menuItemId}`);
@@ -32,23 +28,7 @@ const DiscountManagement: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-900 text-white p-4">
       <div className="container mx-auto">
-        <div className="flex flex-col md:flex-row justify-between mb-4">
-          <select
-            value={filterStatus}
-            onChange={(e) => setFilterStatus(e.target.value)}
-            className="p-2 border border-gray-500 rounded bg-gray-800 text-black focus:outline-none focus:ring-2 focus:ring-yellow-500 transition duration-300"
-          >
-            <option value="All">All</option>
-            <option value="Precentage">Precentage</option>
-            <option value="">Fixed</option>
-          </select>
-          <button
-            onClick={handleAddDiscount}
-            className="rounded-xl text-white bg-gradient-to-r from-orange-600 to-orange-400 hover:from-orange-400 hover:to-orange-600 px-8 py-3"
-          >
-            Add Discount
-          </button>
-        </div>
+        <div className="flex flex-col md:flex-row justify-end mb-4"></div>
         <DiscountTable
           filterStatus={filterStatus}
           toggleEditModal={handleEditDiscount}
@@ -56,6 +36,6 @@ const DiscountManagement: React.FC = () => {
       </div>
     </div>
   );
-};  
+};
 
 export default DiscountManagement;
