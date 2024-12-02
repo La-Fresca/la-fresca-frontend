b8ef66815924447f4b242bb1c765a93a42cc0aad
S Manivannan <suruthi0611@gmail.com>
2024-12-02 13:00:18 +0530

    fix: discount type

--- src/types/discount.ts
+++ src/types/discount.ts
@@ -1,17 +1,13 @@
 export type Discount = {
-    id?: string;
-    name: string;
-    description: string;
-    discountType: string;
-    menuItemType: string;
-    menuItemId: string;
-    amount: number;
-    discountAmount: number;
-    startDate: string;
-    endDate: string;
-    offerDetails: string;
-    cafeId: string;
-  };
-
-   
-   
+  name: string;
+  description: string;
+  discountType: string;
+  menuItemType: string;
+  menuItemId: string;
+  amount: number;
+  discountAmount: number;
+  startDate: string;
+  endDate: string;
+  offerDetails: string;
+  cafeId: string;
+};   
